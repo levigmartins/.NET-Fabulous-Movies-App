6337c93cf66890bd1fdef3f968c603756b950aab
Levi Martins <levi.gm99@gmail.com>
2021-09-28 03:29:57 -0300

    'HelloWorld' View

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -5,12 +5,8 @@ namespace MvcMovies.Controllers {
     
     public class HelloWorldController : Controller {
 
-        public string Index() { // index method is the default called if a method name isn't explicitly specified
-            return "This is my default action...";            
-        }
-
-        public string Welcome(string name, int ID = 1) {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        public IActionResult Index() { // index method is the default called if a method name isn't explicitly specified
+            return View();
         }
 
     }
